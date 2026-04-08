f9a6014759790280bfcc272b30992df1ad5c5fde
digitexcel <digitexcel4@gmail.com>
2026-04-08 18:02:19 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,16 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5173
-  },
-  preview: {
-    host: true,
-    port: 4173
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false
-  }
+  base: '/ielts-2026-cloudflare/'
 })
